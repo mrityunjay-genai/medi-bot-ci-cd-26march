0475385c2c99a8cae227e94c34f56d0898fb6420
mrityunjay-genai <mritytunjay.genai@gmail.com>
2026-03-28 05:20:53 +0530

    update code

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 from flask import Flask, render_template, jsonify, request
 from src.helper import download_hugging_face_embeddings
 from langchain_pinecone import PineconeVectorStore
-# Replace OpenAI with ChatGroq
-# its the latest
-# this is hello
 from langchain_groq import ChatGroq 
-# from langchain.chains import create_retrieval_chain
 from langchain_classic.chains import create_retrieval_chain
-# from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_classic.chains.combine_documents import create_stuff_documents_chain
-
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 from src.prompt import *
